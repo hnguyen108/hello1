bc20c67ed0e22afad049e9c341dda624080a47e9
Hoa Nguyen <hnguyen108@huskers.unl.edu>
2021-08-26 21:46:53 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Hoa Nguyen
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Hoa Nguyen\n");
+
+     return 0;
+ }
